32946187e3f296acaab95fa0ea79c3a06676af0f
Herbert Fernandez Tamayo <hftamayo@gmail.com>
2020-06-30 21:24:20 -0600

    intro a clases

--- app/src/main/java/com/tamayo/basicsalutation/Main.kt
+++ app/src/main/java/com/tamayo/basicsalutation/Main.kt
@@ -1,81 +1,17 @@
 package com.tamayo.basicsalutation
 
 fun main(args: Array<String>){
-    println("Hola a todos")
-    println("primer programa en Kotlin")
+    val tim = Player("tim")
+//    println(tim.name)
+//    println(tim.lives)
+//    println(tim.level)
+//    println(tim.score)
+    tim.show()
 
-    val nombre = "Herbert Fernandez Tamayo"
-    println(nombre)
+    val louise = Player("louise", 5)
+    //louise.level = 5
+    louise.show()
 
-    var salarioSemanal: Int = 100
-    var salamrioMensual: Int = salarioSemanal * 4
-    println(salarioSemanal)
-    println(salamrioMensual)
-
-    println()
-
-    val apples = 6
-    val oranges = 5
-    val fruits: Int = apples + oranges
-    println(fruits)
-
-    println()
-    val weeks: Int = 138
-    val years: Double = weeks / 52.0
-    //concatenaciones
-    println("years entre weeks son: " + years)
-    println("ejemplo de concatenacion: " + nombre)
-
-    //string interpolation
-    println("1/4 de las manzanas disponiles es ${apples / 4}")
-    println("otro ejemplo de concatenar $nombre")
-    println("$weeks semanas traducidas a anios son: $years anios")
-
-    //caracter escaping
-    println("mi nombre es \$nombre")
-
-    //var = variable, val = constante
-
-    /*idealmente el codigo no deberia tener warnings
-    que se ubican en la parte derecha
-     */
-
-    //condicionales
-    val lives = 3
-
-    var isGameOver = (lives < 1)
-
-    if(isGameOver){
-        println("Game is over")
-    } else {
-        println("the player is till alive")
-    }
-
-    println("How old are you?")
-    val age = readLine()!!.toInt()
-    println("Your age is $age")
-
-    val message: String
-    message = when {
-        age < 18 -> {
-            "you are too young to vote"
-        }
-        age == 100 -> {
-            "congratulations"
-        }
-        else -> {
-            "you can vote"
-        }
-    }
-
-//    message = if(age < 18){
-//        "you are too young to vote"
-//    } else if(age == 100){
-//        "congratulations"
-//    } else {
-//        "you can vote"
-//    }
-    println(message)
 
 
 }
---
+++ app/src/main/java/com/tamayo/basicsalutation/Practicas.kt
@@ -0,0 +1,80 @@
+package com.tamayo.basicsalutation
+
+class Practicas {
+    println("Hola a todos")
+    println("primer programa en Kotlin")
+
+    val nombre = "Herbert Fernandez Tamayo"
+    println(nombre)
+
+    var salarioSemanal: Int = 100
+    var salamrioMensual: Int = salarioSemanal * 4
+    println(salarioSemanal)
+    println(salamrioMensual)
+
+    println()
+
+    val apples = 6
+    val oranges = 5
+    val fruits: Int = apples + oranges
+    println(fruits)
+
+    println()
+    val weeks: Int = 138
+    val years: Double = weeks / 52.0
+    //concatenaciones
+    println("years entre weeks son: " + years)
+    println("ejemplo de concatenacion: " + nombre)
+
+    //string interpolation
+    println("1/4 de las manzanas disponiles es ${apples / 4}")
+    println("otro ejemplo de concatenar $nombre")
+    println("$weeks semanas traducidas a anios son: $years anios")
+
+    //caracter escaping
+    println("mi nombre es \$nombre")
+
+    //var = variable, val = constante
+
+    /*idealmente el codigo no deberia tener warnings
+    que se ubican en la parte derecha
+     */
+
+    //condicionales
+    val lives = 3
+
+    var isGameOver = (lives < 1)
+
+    if(isGameOver){
+        println("Game is over")
+    } else {
+        println("the player is till alive")
+    }
+
+    println("How old are you?")
+    val age = readLine()!!.toInt()
+    println("Your age is $age")
+
+    val message: String
+    message = when {
+        age < 18 -> {
+            "you are too young to vote"
+        }
+        age == 100 -> {
+            "congratulations"
+        }
+        else -> {
+            "you can vote"
+        }
+    }
+
+//    message = if(age < 18){
+//        "you are too young to vote"
+//    } else if(age == 100){
+//        "congratulations"
+//    } else {
+//        "you can vote"
+//    }
+    println(message)
+
+}
