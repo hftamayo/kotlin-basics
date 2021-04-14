dfe59623b3cd13b3e63d6949cb96ae82b842eed3
hftamayo <hftamayo@gmail.com>
2021-04-14 14:14:03 -0600

    updating android sdk path

--- app/src/main/java/com/tamayo/basicsalutation/Main.kt
+++ app/src/main/java/com/tamayo/basicsalutation/Main.kt
@@ -16,11 +16,11 @@ fun main(args: Array<String>){
     val RedPotion = Loot("Red Potion", LootType.POTION, 7.50)
     tim.inventory.add(RedPotion)
     //clase 37: for loops
-    val chestArmor = Loot(name: "+3 Chest Armor", LootType.ARMOR, value: 80.00)
+    val chestArmor = Loot("+3 Chest Armor", LootType.ARMOR,  80.00)
     tim.inventory.add(chestArmor)
     tim.showInventory()
-    tim.inventory.add(Loot(name: "Ring of Protection +2 ", LootType.RING, value: 40.25))
-    tim.inventort.add(Loot("Invisibility Potion", LootType.POTION, 35.95))
+    tim.inventory.add(Loot("Ring of Protection +2 ", LootType.RING, 40.25))
+    tim.inventory.add(Loot("Invisibility Potion", LootType.POTION, 35.95))
     tim.showInventory()
     println(tim.toString())
 }
